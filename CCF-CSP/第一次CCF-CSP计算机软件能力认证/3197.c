7b6286c2251b1fd4872b1f6c8f1077946012ed76
yinhan233 <q2543129924@gmail.com>
2026-02-15 22:50:43 +0800

    half of 1st

---
+++ CCF-CSP/第一次CCF-CSP计算机软件能力认证/3197.c
@@ -0,0 +1,28 @@
+// https://www.acwing.com/file_system/file/content/whole/index/content/4186127/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int hash[2002];
+    memset(hash, 0, sizeof(hash));
+    int n;
+    int cnt = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        scanf("%d", &j);
+        if (-1000 <= j && j <= 1000)
+        {
+            hash[j + 1000]++;
+        }
+    }
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (hash[1000 + i] != 0 && hash[1000 - i] != 0)
+        {
+            cnt += (hash[1000 + i] >= hash[1000 - i] ? hash[1000 + i] : hash[1000 - i]);
+        }
+    }
+    printf("%d", cnt);
+}
